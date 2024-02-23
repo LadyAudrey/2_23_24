f17e63eb59733e16e53dc6bd7797c804482dca58
LadyAudrey <theaudreygreene@gmail.com>
2024-02-23 15:55:01 -0800

    accepting state from input

--- Frontend/2.23.24/src/App.jsx
+++ Frontend/2.23.24/src/App.jsx
@@ -4,11 +4,15 @@ import "./index.css";
 
 import { NewTask } from "./Components/NewTask";
 
-const tasks = {
-  TaskName: [],
+let dummyTasks = {
+  deploySite: ["get fetches working", "create backend", "create UI"],
+  getFetchesWorking: ["create backend", "createUI"],
+  createUI: [],
+  createBackend: [],
 };
 
 function App() {
+  const [tasks, setTasks] = useState(dummyTasks);
   return (
     <div className="h-screen w-screen bg-red-900">
       <h1 className="bg-orange-700">Welcome to Your</h1>
@@ -17,7 +21,7 @@ function App() {
         <button>Add a New Task</button>
         <button>Onward!</button>
       </div>
-      <NewTask tasks={tasks} />
+      <NewTask />
     </div>
   );
 }
--- Frontend/2.23.24/src/Components/NewTask.jsx
+++ Frontend/2.23.24/src/Components/NewTask.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 
-export function NewTask(props) {
-  const tasks = props;
-  const [newTask, setNewTask] = useState("");
+let tasks = {
+  deploySite: ["get fetches working", "create backend", "create UI"],
+  getFetchesWorking: ["create backend", "createUI"],
+  createUI: [],
+  createBackend: [],
+};
+
+export function NewTask() {
+  //   const tasks = props;
+  let [newTask, setNewTask] = useState("");
   const [hasPrerequisite, setHasPrerequisite] = useState(false);
 
-  async function handleNewName() {
-    await fetch("http://localhost:3001/", {
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-      body: JSON.stringify(tasks),
-    });
+  //   deleted the async to avoid errorsS
+  function handleNewName() {
+    // console.log("I would send something, but it won't go anywhere");
+    // const newTaskName = newTask;
+    tasks[newTask] = [];
+    console.log(tasks);
+    // await fetch("http://localhost:3001/", {
+    //   headers: { "Content-Type": "application/json" },
+    //   method: "POST",
+    //   body: JSON.stringify(tasks),
+    // });
   }
   return (
     <>
