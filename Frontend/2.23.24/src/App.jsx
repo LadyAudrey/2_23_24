4ebad5155631ddb734c0c982968a904f1d0159ea
LadyAudrey <theaudreygreene@gmail.com>
2024-02-23 14:48:04 -0800

    created fetch

--- Frontend/2.23.24/src/App.jsx
+++ Frontend/2.23.24/src/App.jsx
@@ -2,18 +2,48 @@ import { useState } from "react";
 import "./App.css";
 import "./index.css";
 
+const tasks = {
+  TaskName: [],
+};
+
 function App() {
+  const [newTask, setNewTask] = useState("");
+  const [hasPrerequisite, setHasPrerequisite] = useState(false);
+
+  async function handleNewName() {
+    await fetch("http://localhost:3001/", {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify(tasks),
+    });
+  }
+
   return (
     <div className="h-screen w-screen bg-red-900">
       <h1 className="bg-orange-700">Welcome to Your</h1>
       <h2>Workflow Wizard</h2>
       <div>
-        <h3>Add Your Tasks</h3>
-        <h3>Does it have Prerequisites?</h3>
+        <button>Add a New Task</button>
+        <button>Onward!</button>
       </div>
       <div>
-        <button>Add a new Task</button>
-        <button>Onward!</button>
+        <input
+          autoFocus
+          value={newTask}
+          className=""
+          onChange={(event) => {
+            setNewTask(event.target.value);
+          }}
+          onBlur={handleNewName}
+        />
+        <input
+          value={hasPrerequisite}
+          type="checkbox"
+          className=""
+          onChange={() => {
+            setHasPrerequisite(!hasPrerequisite);
+          }}
+        />
       </div>
     </div>
   );
